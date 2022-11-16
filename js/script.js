0495ff3625ffb38e488f9153057aec49f37d1e6b
RominaMalimban <romina.lyn@hotmail.it>
2022-11-16 15:33:01 +0100

    creazione funzione per click su btn

--- js/script.js
+++ js/script.js
@@ -4,10 +4,6 @@
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 
-// clicco su bottone e il main passa da display none a block:
-
-
-
 // creo le celle quindi:
 
 // 1- prendo il div container:
@@ -21,3 +17,13 @@ for (let i = 1; i <= 100; i++){
     element.classList.add("box");
     container.append(element);
 }
+
+// clicco su bottone e il main passa da display none a block:
+let button = document.querySelector("button");
+let main = document.querySelector("main");
+
+button.addEventListener("click",
+    function(){
+        main.style.display = "block"
+    }
+);
