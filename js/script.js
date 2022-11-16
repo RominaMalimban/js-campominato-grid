b61ab973e850493542c4817f056a64d2cefa55a7
RominaMalimban <romina.lyn@hotmail.it>
2022-11-16 15:22:43 +0100

    creazione griglia e singole celle su js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+// clicco su bottone e il main passa da display none a block:
+
+
+
+// creo le celle quindi:
+
+// 1- prendo il div container:
+let container = document.querySelector(".container");
+
+// 2- mi servo del ciclo per crearmi le boxes e aggiungo la classe da CSS:
+for (let i = 1; i <= 100; i++){
+
+    let element = document.createElement("div");
+    element.append(i);
+    element.classList.add("box");
+    container.append(element);
+}
