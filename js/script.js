90d525e39669af7ffd79ce0898df295c6cf847ed
RominaMalimban <romina.lyn@hotmail.it>
2022-11-17 12:23:07 +0100

    correzione esercizio

--- js/script.js
+++ js/script.js
@@ -3,36 +3,46 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-// clicco su bottone e il main passa da display none a block:
+// variabili manipolazione DOM:
 let button = document.querySelector("button");
 let main = document.querySelector("main");
+let container = document.querySelector(".container");
 
+// al click su bottone mi genero la griglia:
 button.addEventListener("click",
     function(){
-        main.style.display = "block"
+        
+        // 1- prendo il div container e lo svuoto ogni volta al click:
+        container.innerHTML = "";
+
+        // 2- mi servo del ciclo per generarmi 64 boxes tramite la funzione:
+        for (let i = 1; i <= 100; i++){
+
+            let cella = newElement();
+
+            // aggancio la variabile cella al suo contenitore e ci inserisco il numero:
+            container.append(cella);
+            cella.innerHTML = i;
+
+            // devo creare una funzione in cui al click sulla cella, questa si colora:
+            cella.addEventListener("click",
+                function(){
+                    cella.classList.add("clicked-box");
+                    console.log(i);
+                }
+            );
+        }
+
     }
 );
 
-// creo le celle quindi:
 
-// 1- prendo il div container:
-let container = document.querySelector(".container");
+// FUNCTIONS:
 
-// 2- mi servo del ciclo per crearmi le boxes e aggiungo la classe da CSS:
-for (let i = 1; i <= 100; i++){
-
+// Funzione per creare la cella:
+function newElement(){
     let element = document.createElement("div");
-    element.append(i);
+
     element.classList.add("box");
-    container.append(element);
-
-    // devo creare una funzione in cui al click sul quadrato, questo si colora:
-    element.addEventListener("click",
-        function(){
-            element.classList.toggle("clicked-box");
-            console.log(i);
-        }
-        
-    );
+    return element;
 }
-
